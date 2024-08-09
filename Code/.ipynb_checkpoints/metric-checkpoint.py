a7116d17e44f70213abf21556888153dc82b4083
Mengling He <mhe8@login2.cn.isaac.utk.edu>
2024-08-08 23:01:29 -0400

    update the gut files

--- Code/.ipynb_checkpoints/metric-checkpoint.py
+++ Code/.ipynb_checkpoints/metric-checkpoint.py
@@ -12,10 +12,10 @@ micro score is better for unbalanced data
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from sklearn import metrics
+from sklearn.metrics import auc, roc_auc_score, roc_curve, accuracy_score, precision_score, recall_score, f1_score, matthews_corrcoef
+from sklearn.metrics import RocCurveDisplay, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-from sklearn.metrics import roc_auc_score
 
 def roc_auc_score_multiclass(actual_class, pred_class, average = "macro"):
     
@@ -38,18 +38,6 @@ def roc_auc_score_multiclass(actual_class, pred_class, average = "macro"):
     return roc_auc_dict
 
 
-def confusion_matrix2(y_true, y_pred):
-    plt.figure(figsize = (18,8))
-    unique_labels = np.unique(y_true)
-    sns.heatmap(metrics.confusion_matrix(y_true, y_pred), annot = True,fmt='d',
-                annot_kws={"size": 18},xticklabels = unique_labels, yticklabels = unique_labels, cmap = 'summer')
-    plt.xlabel('Predicted Labels')
-    plt.ylabel('True Labels')
-    plt.xticks(fontsize=16)  # Change the size of x-tick labels
-    plt.yticks(fontsize=16)
-    plt.show()
-
-    
 
 def accuracy(y_true, y_pred):
     
@@ -58,7 +46,6 @@ def accuracy(y_true, y_pred):
     -> param y_true: list of true values
     -> param y_pred: list of predicted values
     -> return: accuracy score
-    
     """
     
     # Intitializing variable to store count of correctly predicted classes
@@ -350,4 +337,135 @@ def mcc_score(y_true, y_pred):
     mcc /= num_classes
     
     return mcc
+
+def mcc_score(y_true, y_pred):
+    # find the number of classes
+    num_classes = len(np.unique(y_true))
+
+    # initialize precision to 0
+    mcc = 0
+    
+    # loop over all classes
+    for class_ in list(np.unique(y_true)):
+        
+        # all classes except current are considered negative
+        temp_true = [1 if p == class_ else 0 for p in y_true]
+        temp_pred = [1 if p == class_ else 0 for p in y_pred]
+        
+        
+        # compute tp,tn,fp,fn for current class
+        tp = true_positive(temp_true, temp_pred)
+        tn = true_negative(temp_true, temp_pred)
+        fp = false_positive(temp_true, temp_pred)
+        fn = false_negative(temp_true, temp_pred)
+        
+        
+        # compute precision for current class
+        temp_mcc = ((tp*tn)-(fp*fn))/np.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
+        # keep adding precision for all classes
+        mcc += temp_mcc
+        
+    # calculate and return average precision over all classes
+    mcc /= num_classes
+    
+    return mcc
+
+
+
+
+def confusion_matrix2(y_true, y_pred):
+    plt.figure(figsize = (18,8))
+    unique_labels = np.unique(y_true)
+    sns.heatmap(metrics.confusion_matrix(y_true, y_pred), annot = True,fmt='d',
+                annot_kws={"size": 18},xticklabels = unique_labels, yticklabels = unique_labels, cmap = 'summer')
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.xticks(fontsize=16)  # Change the size of x-tick labels
+    plt.yticks(fontsize=16)
+    plt.show()
+
+
+def plot_confusion_matrices(y, y_pred,title,pos_y=1):
+    acc = accuracy(y, y_pred)
+    precision = precision_score(y, y_pred,pos_label=pos_y)
+    recall =recall_score(y, y_pred,pos_label=pos_y)
+    #f1 = f1_score(y, y_pred,pos_label=pos_y)
+    mcc = matthews_corrcoef(y, y_pred)
+
+    disp = ConfusionMatrixDisplay.from_predictions(
+        y_true=y,
+        y_pred=y_pred,
+        display_labels=sorted(set(y)),  # Use sorted set of labels to ensure consistency
+        cmap='Blues'
+    )
+    
+    # Add metrics below the confusion matrix
+    metrics_text = (f"Accuracy: {acc:.4f}\n"
+                    f"Precision: {precision:.4f}\n"
+                    f"Recall: {recall:.4f}\n"
+                    #f"F1: {f1:.4f}\n"
+                    f"Matthew’s correlation coefficient : {mcc:.4f}\n")
+    disp.ax_.text(0.5, -0.2, metrics_text, ha='center', va='top', fontsize=12, transform=disp.ax_.transAxes)
+
+    plt.tight_layout()  # Adjust layout to make room for the metrics
+    
+    disp.ax_.set_title(title)
+    plt.show()
+
+    
+def plotmacro_confusion_matrices(y, y_pred,title):
+    acc = accuracy(y, y_pred)
+    precision = macro_precision(y, y_pred)
+    recall =macro_recall(y, y_pred)
+    #f1 = metric.macro_f1(y, y_pred)
+    mcc = mcc_score(y, y_pred)
+
+    disp = ConfusionMatrixDisplay.from_predictions(
+        y_true=y,
+        y_pred=y_pred,
+        display_labels=sorted(set(y)),  # Use sorted set of labels to ensure consistency
+        cmap='Blues'
+    )
+    
+    # Add metrics below the confusion matrix
+    metrics_text = (f"Accuracy: {acc:.4f}\n"
+                    f"Precision: {precision:.4f}\n"
+                    f"Recall: {recall:.4f}\n"
+                    #f"F1: {f1:.4f}\n"
+                    f"Matthew’s correlation coefficient : {mcc:.4f}\n")
+    disp.ax_.text(0.5, -0.2, metrics_text, ha='center', va='top', fontsize=12, transform=disp.ax_.transAxes)
+
+    plt.tight_layout()  # Adjust layout to make room for the metrics
+    
+    disp.ax_.set_title(title)
+    plt.show()
+
+
+def plotmicro_confusion_matrices(y, y_pred,title):
+    acc = accuracy(y, y_pred)
+    precision = micro_precision(y, y_pred)
+    recall =micro_recall(y, y_pred)
+    #f1 = metric.micro_f1(y, y_pred)
+    mcc = mcc_score(y, y_pred)
+
+    disp = ConfusionMatrixDisplay.from_predictions(
+        y_true=y,
+        y_pred=y_pred,
+        display_labels=sorted(set(y)),  # Use sorted set of labels to ensure consistency
+        cmap='Blues'
+    )
+    
+    # Add metrics below the confusion matrix
+    metrics_text = (f"Accuracy: {acc:.4f}\n"
+                    f"Precision: {precision:.4f}\n"
+                    f"Recall: {recall:.4f}\n"
+                    #f"F1: {f1:.4f}\n"
+                    f"Matthew’s correlation coefficient : {mcc:.4f}\n")
+    disp.ax_.text(0.5, -0.2, metrics_text, ha='center', va='top', fontsize=12, transform=disp.ax_.transAxes)
+
+    plt.tight_layout()  # Adjust layout to make room for the metrics
+    
+    disp.ax_.set_title(title)
+    plt.show()
+
 
